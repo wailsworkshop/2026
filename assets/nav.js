de2615963046996740a7193a8292647a4d91a9a2
MarcoTocco <marco.tocco@unica.it>
2026-04-23 20:11:21 +0200

    Added mobile burger Menu and updated logo format

--- assets/nav.js
+++ assets/nav.js
@@ -1,29 +1,110 @@
 const nav = `
 <nav>
   <a class="nav-logo" href="index.html">
-  <img src="img/logo/logo.png" alt="WAILS 2026" style="height:52px;">
+    <img src="img/logo/logo.svg" alt="WAILS 2026">
   </a>
+
   <ul class="nav-links">
     <li><a href="index.html#about">About</a></li>
     <li><a href="cfp.html">Call for Papers</a></li>
     <li><a href="index.html#keynotes">Keynotes</a></li>
     <li><a href="committee.html">Committee</a></li>
+
     <li class="has-dropdown">
       <a href="#">Attend</a>
-      <div class="dropdown"><div class="dropdown-inner">
-        <a href="attend-registration.html">Registration</a>
-        <a href="attend-travel.html">Travel Information</a>
-      </div></div>
+      <div class="dropdown">
+        <div class="dropdown-inner">
+          <a href="attend-registration.html">Registration</a>
+          <a href="attend-travel.html">Travel Information</a>
+        </div>
+      </div>
     </li>
+
     <li class="has-dropdown">
       <a href="#">Schedule</a>
-      <div class="dropdown"><div class="dropdown-inner">
-        <a href="schedule-programme.html">Programme</a>
-        <a href="schedule-accepted-papers.html">Accepted Papers</a>
-      </div></div>
+      <div class="dropdown">
+        <div class="dropdown-inner">
+          <a href="schedule-programme.html">Programme</a>
+          <a href="schedule-accepted-papers.html">Accepted Papers</a>
+        </div>
+      </div>
     </li>
+
     <li><a href="cfp.html" class="nav-cta">Submit</a></li>
   </ul>
+
+  <button class="nav-toggle" aria-label="Open menu" aria-expanded="false" aria-controls="mobile-menu">
+    <span></span>
+    <span></span>
+    <span></span>
+  </button>
+
+  <div class="nav-mobile" id="mobile-menu">
+    <a href="index.html#about">About</a>
+    <a href="cfp.html">Call for Papers</a>
+    <a href="index.html#keynotes">Keynotes</a>
+    <a href="committee.html">Committee</a>
+
+    <div class="nav-mobile-group">
+      <div class="nav-mobile-label">Attend</div>
+      <a href="attend-registration.html">Registration</a>
+      <a href="attend-travel.html">Travel Information</a>
+    </div>
+
+    <div class="nav-mobile-group">
+      <div class="nav-mobile-label">Schedule</div>
+      <a href="schedule-programme.html">Programme</a>
+      <a href="schedule-accepted-papers.html">Accepted Papers</a>
+    </div>
+
+    <a href="cfp.html" class="nav-mobile-cta">Submit</a>
+  </div>
 </nav>`;
 
 document.body.insertAdjacentHTML("afterbegin", nav);
+
+const toggle = document.querySelector(".nav-toggle");
+const mobileMenu = document.querySelector(".nav-mobile");
+const mobileLinks = document.querySelectorAll(".nav-mobile a");
+
+function closeMenu() {
+  mobileMenu.classList.remove("is-open");
+  toggle.classList.remove("is-open");
+  toggle.setAttribute("aria-expanded", "false");
+  document.body.classList.remove("menu-open");
+}
+
+function openMenu() {
+  mobileMenu.classList.add("is-open");
+  toggle.classList.add("is-open");
+  toggle.setAttribute("aria-expanded", "true");
+  document.body.classList.add("menu-open");
+}
+
+if (toggle && mobileMenu) {
+  toggle.addEventListener("click", () => {
+    const isOpen = mobileMenu.classList.contains("is-open");
+    if (isOpen) {
+      closeMenu();
+    } else {
+      openMenu();
+    }
+  });
+
+  mobileLinks.forEach((link) => {
+    link.addEventListener("click", closeMenu);
+  });
+
+  document.addEventListener("click", (e) => {
+    const navEl = document.querySelector("nav");
+    if (navEl && !navEl.contains(e.target)) {
+      closeMenu();
+    }
+  });
+
+  window.addEventListener("resize", () => {
+    if (window.innerWidth > 900) {
+      closeMenu();
+    }
+  });
+}
